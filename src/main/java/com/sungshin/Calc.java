3d862b1e83dee9da9ac40145ae7f3ab539f507cb
EunchaeKim <kitsch0728@naver.com>
2017-02-08 11:29:40 +0900

    update

--- src/main/java/com/sungshin/Calc.java
+++ src/main/java/com/sungshin/Calc.java
@@ -1,13 +1,13 @@
 package com.sungshin;
 
 public class Calc {
-	public int add(int x, int y) {
-		System.out.println("add func");
-		return x+y;
-	}
+    public int add(int x, int y) {
+        System.out.println("add func");
+        return x+y;
+    }
 
-	public int sub(int x, int y) {
-		return x+y'
-	}
+    public int sub(int x, int y) {
+        return x+y;
+    }
 
 }
